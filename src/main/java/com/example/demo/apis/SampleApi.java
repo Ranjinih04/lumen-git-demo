4544b16e4f76d4f1bc5817f79a33be8750f0845a
H Ranjini <ranjini.h@lumen.com>
2024-01-10 14:16:16 +0530

    response changed

--- src/main/java/com/example/demo/apis/SampleApi.java
+++ src/main/java/com/example/demo/apis/SampleApi.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleApi {
     @GetMapping
     public ResponseEntity<String> get(){
-        return ResponseEntity.ok("Hello world");
+        return ResponseEntity.ok("Hello master");
     }
 
 }
